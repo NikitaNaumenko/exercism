421873d91c0296310c427f267ba999f1da6eaae5
Naumenko Nikita <naumenkoniki@gmail.com>
2019-02-18 22:51:45 +0300

    Fix bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,11 +1,17 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      String.trim(input) == "" -> "Fine. Be that way!"
-      String.upcase(input) == input -> "Whoa, chill out!"
-      String.ends_with?(input, "?") -> "Sure."
-      String.upcase(input) == input && String.ends_with?(input, "?") -> " Calm down, I know what I'm doing!"
+      silent?(input) -> "Fine. Be that way!"
+      shouting_question?(input) -> "Calm down, I know what I'm doing!"
+      shouting?(input) -> "Whoa, chill out!"
+      question?(input) -> "Sure."
       true -> "Whatever."
     end
   end
+
+  defp silent?(input), do: "" == String.trim(input)
+  defp shouting?(input), do: input == String.upcase(input) && letters?(input)
+  defp question?(input), do: String.ends_with?(input, "?")
+  defp shouting_question?(input), do: shouting?(input) && question?(input)
+  defp letters?(input), do: Regex.match?(~r/\p{L}+/, input)
 end
