60b9dfc5fd2052ddff7dc2dff2539bd53b951410
Naumenko Nikita <naumenkoniki@gmail.com>
2019-06-02 11:28:43 +0300

    Fix pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class Pangram
-  ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z].freeze
+  ALPHABET = %w[
+    q w e r t y u i o p
+    a s d f g h j k l
+    z x c v b n m
+  ].freeze
 
   def self.pangram?(sentence)
     downcased_sentence = sentence.downcase
