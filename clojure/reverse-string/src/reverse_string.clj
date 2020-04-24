444d8c78b1ad2ed56068c333a96968e1f9527b17
Nikita Naumenko <naumenkoniki@gmail.com>
2020-04-24 11:04:35 +0300

    add reverse-string clojure

---
+++ clojure/reverse-string/src/reverse_string.clj
@@ -0,0 +1,11 @@
+(ns reverse-string)
+
+(defn reverse-string [s]
+  (loop [lastIndex (- (count s) 1)
+         result ""]
+    (if (= (count result) (count s))
+      result
+      (recur (- lastIndex 1)
+             (str result (get s lastIndex))))))
+
+(reverse-string "Jopa")
