9fbfae2da13929cf8f91365b523066652327a498
Nikita Naumenko <naumenkoniki@gmail.com>
2020-05-31 12:46:09 +0300

    fix bob.clj

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,38 +1,19 @@
-<<<<<<< HEAD
-(ns bob)
-
-(defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
-)
-=======
 (ns bob
-  (:require [clojure.string :refer [ends-with? blank? upper-case]]))
-
-(defn- has-alpha?  [s] (some #(Character/isLetter (int %)) s))
-
-(defn- upper-case? [s] (= s (upper-case s)))
-
-(defn- shouting?   [s] (and (upper-case? s)
-                            (has-alpha? s)))
-
-(defn- shouting-question?   [s] (and (upper-case? s)
-                                     (ends-with? s "?")
-                                     (has-alpha? s)))
+  (:require [clojure.string :as string]))
 
 
-(defn- question?   [s] (and (ends-with? s "?")
-                            (not (shouting? s))))
-
-(defn- silence?    [s] (blank? s))
+(defn- shouting?   [s] (re-matches #"[^a-z]*[A-Z][^a-z]*" s))
+(defn- question?   [s] (string/ends-with? (string/trimr s) "?"))
+(defn- shouting-question?   [s] (and (shouting? s)
+                                     (question? s)))
 
 (defn response-for [s]
   (let [st (clojure.string/trim s)]
    (condp apply [st]
-    silence?  "Fine. Be that way!"
+    string/blank? "Fine. Be that way!"
     shouting-question? "Calm down, I know what I'm doing!"
     shouting? "Whoa, chill out!"
     question? "Sure."
               "Whatever.") 
     )
-  )
->>>>>>> add bob cloj
+)
