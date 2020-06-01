7835f8e573e0adf30442e8c3a9da813e7c06afa5
Nikita Naumenko <naumenkoniki@gmail.com>
2020-06-01 10:25:48 +0300

    add more solve

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,14 +1,26 @@
 (ns armstrong-numbers)
 
-(defn num-to-digits [num]
-  (->> num str (map (comp read-string str))))
+; (defn num-to-digits [num]
+;   (->> num str (map (comp read-string str))))
 
-(defn expt [base pow]
+; (defn expt [base pow]
+;   (reduce * 1 (repeat pow base)))
+
+; (defn armstrong? [num]
+;   (let [pow (count (str num))
+;         digits (num-to-digits num)
+;         sum (reduce + (map #(expt % pow) digits))]
+;     (= sum num)))
+
+(defn- num->digits [n]
+  (mapv #(Character/digit ^Character % 10) (str n)))
+
+(defn- expt [base pow]
   (reduce * 1 (repeat pow base)))
 
 (defn armstrong? [num]
-  (let [pow (count (str num))
-        digits (num-to-digits num)
-        sum (reduce + (map #(expt % pow) digits))]
-    (= sum num)))
-
+  (let [digits (num->digits num)
+        pow    (count digits)]
+    (->> (map #(expt % pow) digits)
+         (apply +) ;; apply works too.
+         (= num))))
