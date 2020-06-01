71ba5705d986e6e9b6d88b353214f64665d642b6
Nikita Naumenko <naumenkoniki@gmail.com>
2020-06-01 18:29:36 +0300

    fix rna

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,18 +1,31 @@
 (ns rna-transcription)
 
-(def transcription-map
-  {\C \G
+; (def transcription-map
+;   {\C \G
+;    \G \C
+;    \A \U
+;    \T \A})
+
+; (defn find-n [n]
+;   (or (get transcription-map n)
+;       (throw (AssertionError. "Error wrong nucleotid"))))
+
+; (defn to-rna [dna]
+;   (->> dna
+;         (map #(find-n %))
+;         (apply str)))
+
+
+(def ^:private ^:const transcriptions
+  {\A \U
+   \C \G
    \G \C
-   \A \U
    \T \A})
 
-(defn find-n [n]
-  (or (get transcription-map n)
-      (throw (AssertionError. "Error wrong nucleotid"))))
+;; `defn-` is the same as `defn ^:private`. Unfortunately, there's no `def-`.
+(defn- transcribe [nucleotide]
+  (or (transcriptions nucleotide)
+      (throw (AssertionError. (str "Invalid nucleotide: " nucleotide)))))
 
 (defn to-rna [dna]
-  (->> dna
-        (map #(find-n %))
-        (apply str)))
-
-
+  (apply str (map transcribe dna)))
