742db342b06a22c4c42d017d818262c539aebc6a
Nikita Naumenko <naumenkoniki@gmail.com>
2020-06-10 17:50:42 +0300

    refactoring run length encoding

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -3,18 +3,20 @@
 (defn run-length-encode
   "encodes a string with run-length-encoding"
   [plain-text]
-  (apply str
-    (for [x (partition-by identity plain-text)]
-      (str
-        (when-not (= 1 (count x)) (count x)) (first x)))))
+  (let [parts (partition-by identity plain-text)]
+    (->> parts
+     (map #(str (when-not (= 1 (count %)) (count %)) (first %)))
+     (apply str))))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
   [cipher-text]
-  (->> cipher-text
-    (re-seq #"[0-9]+.|[a-zA-Z\s]")
-    (map #(if (= (count %) 1) (str "1" %) %))
-    (map #(list (Integer. (reduce str "" (butlast %))) (str (last %))))
-    (map #(apply str (repeat (first %) (second %))))
+   (->> cipher-text
+    (re-seq #"(\d+)?(\D)")
+    (map (fn [[_ number character :as arg]]
+           (if-not number [(str "1" character) "1" character] arg)))
+    (map (fn ([[_ number character]]
+              [(Long/valueOf number) character])))
+    (map (fn [[number character]]
+           (apply str (repeat number character))))
     (apply str)))
-
