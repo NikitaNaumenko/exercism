c6fe99976a2b1537946fbe6288c366baf4ec7cfd
Nikita Naumenko <naumenkoniki@gmail.com>
2020-06-14 21:32:48 +0300

    fix isbn

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,17 +1,19 @@
 (ns isbn-verifier)
 
-(defn is-in? [xs x] (some #(= x %) xs))
+(defn isbn-chars [s]
+  (filter #(contains? #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \X} %) s))
 
-(defn isbn-chars [isbn]
-  (filter #(is-in? [\0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \X] %) isbn))
+(defn checksum? [num]
+  (-> num
+      (mod 11)
+      (zero?)))
 
 (defn isbn? [isbn]
   (let [chars (isbn-chars isbn)
-        nums  (map #(if (= \X %) 10 (Character/digit % 10)) chars)]
-    (and (-> chars butlast (is-in? \X) not)
-         (= 10 (count chars))
-         (as-> nums x
-           (map #(* %1 %2) (range 10 0 -1) x)
-           (reduce + x)
-           (mod x 11)
-           (zero? x)))))
+        nums (map #(if (= \X %) 10 (Character/digit ^Character % 10)) chars)]
+    (and (or (re-matches #"(?:\d-?){9}[\dX]" isbn) false)
+         (->> nums
+           (map * (range 10 0 -1))
+           (reduce +)
+           (checksum?)))))
+
