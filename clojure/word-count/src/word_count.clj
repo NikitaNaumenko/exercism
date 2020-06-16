0839127643beef05c0833b6aaa6d7948ca74f881
Nikita Naumenko <naumenkoniki@gmail.com>
2020-06-16 16:12:20 +0300

    fix word count

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -2,9 +2,7 @@
   (:require [clojure.string :as str]))
 
 (defn word-count [s]
-  (let [words (map str/lower-case (re-seq #"\w+|\d" s))]
-    (reduce (fn [acc word]
-              (assoc acc word (if (contains? acc word)
-                                (+ 1 (get acc word))
-                                1))
-              ) {} words)))
+  (->> s
+       (str/lower-case)
+       (re-seq #"\w+")
+       (reduce #(update % %2 (fnil inc 0)) {})))
