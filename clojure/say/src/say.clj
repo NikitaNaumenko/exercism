3387d3840754c8fe9275416a868c80c03d652339
Nikita Naumenko <naumenkoniki@gmail.com>
2020-08-27 21:22:51 +0300

    fix say

--- clojure/say/src/say.clj
+++ clojure/say/src/say.clj
@@ -1,16 +1,17 @@
-(ns say
-  (:require [clojure.string :as str]))
+(ns say)
 
-(def specials
-  {1  "one"
-   2  "two"
-   3  "three"
-   4  "four"
-   5  "five"
-   6  "six"
-   7  "seven"
-   8  "eight"
-   9  "nine"
+(def number->name
+  {
+   0 "zero"
+   1 "one"
+   2 "two"
+   3 "three"
+   4 "four"
+   5 "five"
+   6 "six"
+   7 "seven"
+   8 "eight"
+   9 "nine"
    10 "ten"
    11 "eleven"
    12 "twelve"
@@ -20,60 +21,67 @@
    16 "sixteen"
    17 "seventeen"
    18 "eighteen"
-   19 "nineteen"})
+   19 "nineteen"
+   20 "twenty"
+   30 "thirty"
+   40 "forty"
+   50 "fifty"
+   60 "sixty"
+   70 "seventy"
+   80 "eighty"
+   90 "ninety"
+   })
 
-(def tens
-  {2 "twenty"
-   3 "thirty"
-   4 "forty"
-   5 "fifty"
-   6 "sixty"
-   7 "seventy"
-   8 "eighty"
-   9 "ninety"})
+(defn group-digits [num]
+  (->> (str num)
+       (map #(Character/getNumericValue %1))
+       (reverse)
+       (partition-all 3)
+       (map reverse)
+       (reverse)))
 
-(defn number-without-zero [num]
+(defn unit->name [unit]
+  (number->name unit))
+
+(defn ten->name [ten unit]
   (cond
-    (zero? num) nil
+    (zero? ten)  (unit->name unit)
+    (zero? unit) (number->name (* ten 10))
+    (= 1 ten)    (number->name (+ (* ten 10) unit))
+    :else        (str (number->name (* ten 10)) "-" (unit->name unit))))
 
-    (<= 1 num 19) (get specials num)
+(defn hundred->name [hundred ten unit]
+  (cond
+    (= hundred ten unit 0)         ""
+    (zero? hundred)                (ten->name ten unit)
+    (and (zero? ten) (zero? unit)) (str (number->name hundred)
+                                        " hundred")
+    :else                          (str (number->name hundred)
+                                        " hundred " (ten->name ten unit))))
 
-    (<= 20 num 99)
-    (let [tens-digit (quot num 10)
-          ones-digit (rem num 10)
-          ones-text  (get specials ones-digit)
-          tens-text  (str (get tens tens-digit)
-                          (if (not-empty ones-text) "-"))]
-      (str tens-text ones-text))
+(defn group->names
+  ([unit]             (unit->name unit))
+  ([ten unit]         (ten->name ten unit))
+  ([hundred ten unit] (hundred->name hundred ten unit)))
 
-    (<= 100 num 999)
-    (let [digit            (quot num 100)
-          remaining-digits (rem num 100)
-          remaining-text   (number-without-zero remaining-digits)]
-      (str/join " " [(get specials digit)
-                     "hundred"
-                     remaining-text]))))
+(defn insert-cardinality
+  ([hundred] hundred)
+  ([thousand hundred]                 (if (empty? hundred)
+                                        (str thousand " thousand")
+                                        (str thousand " thousand " (insert-cardinality hundred))))
+  ([million thousand hundred]         (if (empty? (str thousand hundred))
+                                        (str million " million")
+                                        (str million " million " (insert-cardinality thousand hundred))))
+  ([billion million thousand hundred] (if (empty? (str million thousand hundred))
+                                        (str billion " billion")
+                                        (str billion " billion " (insert-cardinality million thousand hundred)))))
+
+(defn translate [number]
+  (let [groups (group-digits number)
+        named-groups (map #(apply group->names %1) groups)]
+    (apply insert-cardinality named-groups)))
 
 (defn number [num]
-  (cond
-    (zero? num)
-    "zero"
-
-    (not (<= 0 num 999999999999))
-    (throw (IllegalArgumentException. "Must be a valid number."))
-
-    :else
-    (let [digits         (str num)
-          num-digits     (count digits)
-          [start rest]   (split-at (mod num-digits 3) digits)
-          groups         (map (comp number-without-zero read-string #(apply str %))
-                              (concat (if-not (empty? start) (list start)) (partition 3 rest)))
-          group-and-size (map vector
-                              (reverse groups)
-                              '(nil "thousand" "million" "billion" "trillion"))]
-      (->> group-and-size
-           (filter #(not-empty (first %)))
-           reverse
-           (apply concat)
-           (str/join " ")
-           str/trim))))
+  (if (or (< num 0) (> num 999999999999))
+    (throw (IllegalArgumentException. ""))
+    (translate num)))
