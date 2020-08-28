5b91d0fb1d6a1f51425d585680c09296c0ca8bd2
Nikita Naumenko <naumenkoniki@gmail.com>
2020-08-28 09:21:21 +0300

    add two_fer go

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -5,11 +5,15 @@
 // https://golang.org/doc/effective_go.html#commentary
 package twofer
 
+import (
+  "fmt"
+)
 // ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+  lengthStr := len(name)
+  if lengthStr == 0 {
+    return "One for you, one for me."
+  } else {
+    return fmt.Sprintf("One for %v, one for me.", name)
+  }
 }
